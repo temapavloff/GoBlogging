97971d001717ac3eaac54abba811d66a851e1670
Artem Pavlov <pavl0ff@ya.ru>
2018-09-02 17:13:48 +0300

    added static file server

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"log"
+	"net/http"
 
 	"GoBlogging/builder"
 	"GoBlogging/config"
@@ -10,6 +12,8 @@ import (
 
 func main() {
 	configPath := flag.String("c", "config.json", "Path to config file")
+	serve := flag.Bool("s", false, "Start server after building complete")
+	port := flag.String("p", "8080", "Port to serve on")
 	flag.Parse()
 
 	c := config.New(*configPath)
@@ -19,4 +23,12 @@ func main() {
 
 	b.Read(builder.Reader)
 	b.Write(w)
+
+	if *serve {
+		dir := c.GetAbsPath(c.Output)
+
+		http.Handle("/", http.FileServer(http.Dir(dir)))
+		log.Printf("Serving %s on HTTP port: %s\n", dir, *port)
+		log.Fatal(http.ListenAndServe(":"+*port, nil))
+	}
 }
